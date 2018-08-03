934d181fd3f6d96611ee507a234e9210ff94e327
Henrique Rangel <henrirangel@hotmail.com>
2018-08-03 20:24:41 -0300

    Usar Typeahead para pesquisar por usuários e redirecionar para seus perfis

---
+++ client/Cabecalho/Pesquisa.js
@@ -0,0 +1,12 @@
+Template.Pesquisa.rendered = function() {
+	Meteor.typeahead.inject();
+};
+
+Template.Pesquisa.helpers({
+	items: function() {
+		return Meteor.users.find().fetch().map(function(user){ return {id: user._id, value: user.profile.name}; });
+	},
+	selected: function(event, suggestion, datasetName) {
+		FlowRouter.go("/perfis/" + suggestion.id);
+	}
+});
